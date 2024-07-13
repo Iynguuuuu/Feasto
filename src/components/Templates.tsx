4634fe6829e158f5e81bed7c6e8f4e3b2d2be39d
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-07-13 12:44:52 +0530

    Edited the heading different for each screens

--- src/components/Templates.tsx
+++ src/components/Templates.tsx
@@ -6,12 +6,14 @@ export const Templates = () => {
             <div className="text-center">
                 <p className="text-[14px] md:text-[18px] text-primary font-medium">STARTER TEMPLATES</p>
                 <p className="text-[36px] md:text-[60px] text-text1 font-black relative">
-                    <span className="block md:inline">Kickstart your online stores</span>
+                    <span className="hidden md:inline">Kickstart your online stores</span>
+                    <span className="inline md:hidden">Kickstart your</span>
                     <br className="hidden md:block" />
+                    <span className="inline md:hidden"><br />online stores</span>
                     <span className="hidden md:inline"> with amazing templates</span>
                     <span className="block md:hidden"> with beautiful</span>
                     <span className="block md:hidden"> Templates</span>
-                    <br className="hidden md:block" />
+                
                 </p>
 
                 <Cards />
